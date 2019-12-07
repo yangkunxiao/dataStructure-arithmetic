50adbed4dd9aa18a282d38955a61a475999c28b4
yangkunxiao <15738518697@163.com>
2019-12-08 01:13:38 +0800

    选择排序 冒泡排序

---
+++ src/in-commen/bubble-sort.js
@@ -0,0 +1,25 @@
+/**
+ * 冒泡排序:每次从数组0位开始查找，如果当前值比较大，则交换位置。
+ * 这样外一次循环下来，最大的值就会排入到最后的位置。 
+ * 第二次外循环就可以将第二大的数字找出，拍到倒数第二位
+ * 依次类推，所以内层循环只需循环 (len-i) 次即可
+ * 
+*/
+var arr = [1, 4, 5, 2, 3, 9, 0, 7, 6];
+
+for (let i = 0, len = arr.length; i < len; i++) {
+    for (let j = 0; j < len - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp
+        }
+        console.log(arr);
+
+    }
+
+}
+
+
+
+console.log(arr);
---
+++ src/in-commen/selection-sort.js
@@ -0,0 +1,25 @@
+//选择排序
+var arr = [1, 4, 5, 2, 3, 9, 0, 7, 6];
+var temp;
+/**
+ * 选择排序每次比较的是数组中特定索引的值与全数组中每个值的大小比较，
+ * 每次都选出一个最小(最大)值，如果当前索引的值大于之后索引的值，则两者进行交换
+ * 时间复杂度：O(n^2);
+ * 空间复杂度：O(1)
+ * 
+*/
+for (let i = 0,len = arr.length; i <len; i++) {
+    for (let j = i + 1; j < len - i; j++) {
+        if(arr[i] > arr[j]){
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        } 
+    }
+    
+}
+console.log(arr);
+
+
+
+
